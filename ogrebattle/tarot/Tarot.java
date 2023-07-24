2213761e82756d6d678f221a750dd5e5f0b44f65
aus101 <68292952+aus101@users.noreply.github.com>
2023-07-24 00:22:35 -0400

    Add files via upload

---
+++ ogrebattle/tarot/Tarot.java
@@ -0,0 +1,5 @@
+package ogrebattle.tarot;
+
+public enum Tarot {
+	Magician, Priestess, Empress, Emperor, Hierophant, Lovers, Chariot, Strength, Hermit, Fortune, Justice, Hanged_Man, Death, Temperance, Devil, Tower, Star, Moon, Sun, Judgment, World, Fool
+}
