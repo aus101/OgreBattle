428d38053c97d9dda1adb6733f3d56ba5c3bedfa
aus101 <68292952+aus101@users.noreply.github.com>
2023-08-27 15:15:21 -0400

    Delete Tarot.java

--- ogrebattle/tarot/Tarot.java
+++
@@ -1,5 +0,0 @@
-package ogrebattle.tarot;
-
-public enum Tarot {
-	Magician, Priestess, Empress, Emperor, Hierophant, Lovers, Chariot, Strength, Hermit, Fortune, Justice, Hanged_Man, Death, Temperance, Devil, Tower, Star, Moon, Sun, Judgment, World, Fool
-}
