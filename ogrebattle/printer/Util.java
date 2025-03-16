ca2dc04c6835b0777b789a3da8348453de4aa038
aus101 <aus10@vt.edu>
2025-03-16 15:23:54 -0400

    time and number formatting

--- ogrebattle/printer/Util.java
+++ ogrebattle/printer/Util.java
@@ -2,7 +2,9 @@ package ogrebattle.printer;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.text.NumberFormat;
 import java.util.Arrays;
+import java.util.Locale;
 
 import ogrebattle.tarot.exact.AllPossibleHands;
 import ogrebattle.tarot.pojo.LORD;
@@ -15,6 +17,9 @@ import ogrebattle.tarot.simulate.TarotDeck;
  */
 public class Util {
 	public final static int NANOSECONDS_IN_1_SECOND = 1_000_000_000;
+	private static final BigDecimal ONE_HUNDRED = new BigDecimal(100);
+	private static final NumberFormat COMMAS = NumberFormat.getInstance(Locale.US);
+
 	/**
 	 * Print odds of hand combinations without regard to precision or floating point error<br>
 	 * @param successes hands that matched given criteria
@@ -53,12 +58,33 @@ public class Util {
 	 * @param successes hands that matched given criteria
 	 * @param totalHands total hands possible that were iterated through
 	 * @param precision decimal places, with rounding on last decimal
+	 * @param separator what to use instead of "or 1 in" for 1 in X iterations
 	 * @return String with the odds to print
 	 */
-	public static String percentAlt(int successes, int totalHands, int precision) {
-		BigDecimal num = new BigDecimal(successes);
-		BigDecimal denom = new BigDecimal(totalHands);
-		return (num.multiply(new BigDecimal(100)).divide(denom, precision, RoundingMode.HALF_UP) + "%" + " (" + successes + ")");
+	public static String percentAlt(int successes, long totalHands, int precision, String separator) {
+		StringBuilder sb = new StringBuilder();
+		if (successes > 0) {//avoid awkward "0% (0)" and divide by 0 ArithmeticException
+			BigDecimal num = new BigDecimal(successes);
+			BigDecimal denom = new BigDecimal(totalHands);
+			//PlainString to avoid exponents such as 1E+4 as an output
+			sb.append(num.multiply(ONE_HUNDRED).divide(denom, precision, RoundingMode.HALF_UP)
+					.stripTrailingZeros().toPlainString()).append("% (").append(successes).append(')').append(separator)
+					.append(denom.divide(num, 1, RoundingMode.HALF_UP).stripTrailingZeros().toPlainString());
+		} else {//== 0
+			sb.append("0");
+		}
+		return sb.toString();
+	}
+	
+	/**
+	 * Print odds of hand combinations using BigDecimal to prevent floating point error with the count second<br>
+	 * @param successes hands that matched given criteria
+	 * @param totalHands total hands possible that were iterated through
+	 * @param precision decimal places, with rounding on last decimal
+	 * @return String with the odds to print
+	 */
+	public static String percentAlt(int successes, long totalHands, int precision) {
+		return percentAlt(successes, totalHands, precision, " or 1 in ");
 	}
 	
 	/**
@@ -127,5 +153,78 @@ public class Util {
 	public static void printRandomHands(int hands, AllPossibleHands allHands) {
 		for (int i = 0; i < hands; i++)
 			System.out.println(allHands.returnRandomHandInList());
+	}
+	
+	/**
+	 * @param nanoTime the time as a long in nanoseconds, such as the difference with 2 System.nanoTime() calls
+	 * @return String in human readable minutes and seconds with < 1 second as "less than 1 second"
+	 */
+	public static String nanoToMinutesSeconds(long nanoTime) {
+		long seconds = nanoTime / NANOSECONDS_IN_1_SECOND;
+		StringBuilder sb = new StringBuilder();
+		if (seconds < 60) {
+			if (seconds < 1) {
+				sb.append("less than 1 second");
+			} else if (seconds > 1) {
+				sb.append(seconds + " seconds");
+			} else {//== 1
+				sb.append("1 second");
+			} 
+		} else {
+			long minutes = seconds / 60;
+			long remainder = seconds % 60;
+			if (minutes > 1) {
+				sb.append(minutes).append(" minutes");
+			} else {
+				sb.append(minutes).append(" minute");
+			}
+			if (remainder > 0) {
+				if (remainder > 1) {
+					sb.append(", ").append(remainder).append(" seconds");
+				} else {//== 1
+					sb.append(", 1 second");
+				}
+			}
+		}
+		return sb.toString();
+	}
+	/*
+	 * 43064913176100 717 minutes, 44 seconds
+	 * 43141650708600 719 minutes, 1 second
+	 * 42960766596100 716 minutes
+	 */
+	
+	/**
+	 * @param number the number as an int
+	 * @return number as String with commas spacing
+	 */
+	public static String numberSeparator(int number) {
+		return COMMAS.format(number);
+	}
+	
+	/**
+	 * @param number the number as a long
+	 * @return number as String with commas spacing
+	 */
+	public static String numberSeparator(long number) {
+		return COMMAS.format(number);
+	}
+	
+/**
+ * @param number the number as an int
+ * @param spacer the spacer every 3 digits, i.e. "." returns 10.000 for 10000
+ * @return number as String with custom point spacing
+ */
+	public static String numberSeparatorCustom(int number, String spacer) {
+		return COMMAS.format(number).replaceAll(",", spacer);
+	}
+	
+	/**
+	 * @param number the number as a long
+	 * @param spacer the spacer every 3 digits, i.e. "." returns 10.000 for 10000
+	 * @return number as String with custom point spacing
+	 */
+	public static String numberSeparatorCustom(long number, String spacer) {
+		return COMMAS.format(number).replaceAll(",", spacer);
 	}
 }
